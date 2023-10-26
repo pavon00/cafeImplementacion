0f42786213f8f9977be6d1ddc2077dd5a2c2b4ff
clepnid <clepnid@gmail.com>
2023-10-26 04:30:11 +0200

    replicator hecho

--- src/libreria/task/Replicator.java
+++ src/libreria/task/Replicator.java
@@ -1,11 +1,35 @@
 package libreria.task;
 
+import libreria.Slot;
+
 /*
  * Distribuye los mensajes de entrada hacia las salidas
  * Entradas: 1, Salidas: n
  * 
 */
 
-public class Replicator extends Router{
-
+public class Replicator extends Router {
+	
+	private String buffer;
+	
+	@Override
+	public void run() {
+		// esperar a los nodos de entrada
+		super.run();
+		for (Slot slot : this.getSlotsSalida()) {
+			slot.getPortSalida().setBufferString(this.getBuffer());
+		}
+	}
+	
+	@Override
+	public void setBuffer(String buffer) {
+		// TODO Auto-generated method stub
+		this.buffer = buffer;
+	}
+	
+	@Override
+	public String getBuffer() {
+		// TODO Auto-generated method stub
+		return this.buffer;
+	}
 }
